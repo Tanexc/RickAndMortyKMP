ef03d1cc1b1cb8f49f6fbf5d37c58b3572a98f9b
tanexc <artur.231456@gmail.com>
2024-06-28 00:45:03 +0300

    character data changes

--- shared/src/commonMain/kotlin/ru/tanexc/gravityfallsmultiplatform/data/local/CharacterDao.kt
+++ shared/src/commonMain/kotlin/ru/tanexc/gravityfallsmultiplatform/data/local/CharacterDao.kt
@@ -3,9 +3,17 @@ package ru.tanexc.gravityfallsmultiplatform.data.local
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import ru.tanexc.gravityfallsmultiplatform.core.REQUEST_PAGE_LIMIT
 
 @Dao
 interface CharacterDao {
+
+    @Query("select * from characterentity where id = :id")
+    suspend fun getById(id: Int): CharacterEntity
+
+    @Query("select * from characterentity limit :limit offset :offset")
+    fun get(offset: Int, limit: Int = REQUEST_PAGE_LIMIT): Flow<List<CharacterEntity>>
 
     @Insert
     suspend fun insert(data: List<CharacterEntity>)
